dd42c7d95204fb3d24112779e6d8461ebfa02867
chenhr13 <chenhr13@chinatelecom.cn>
2025-12-16 17:04:32 +0800

    [arch] 命名规范处理

--- src/ct/arch/src/arch.rs
+++ src/ct/arch/src/arch.rs
@@ -1,46 +1,53 @@
 /*
- * Copyright(c) 2022-2024 China Telecom Cloud Technologies Co., Ltd. All rights reserved
- *   syskits is licensed under Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL V2
- * You may obtain a copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
- * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
- * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
- * See the Mulan PSL v2 for more details.
+ *    Copyright(c) 2022-2024 China Telecom Cloud Technologies Co., Ltd. All rights reserved
+ *     syskits is licensed under Mulan PSL v2.
+ *    You can use this software according to the terms and conditions of the Mulan PSL V2
+ *    You may obtain a copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2
+ *    THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *    KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *    NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *    See the Mulan PSL v2 for more details.
  */
 
 use platform_info::*;
 
 #[warn(unused_imports)]
-use clap::{crate_version, Command};
-use ctcore::ct_error::{CTResult, CtSimpleError};
-use ctcore::{ct_format_usage, ct_help_about, ct_help_section};
+use clap::crate_version;
+use clap::Command;
 
-static CT_ABOUT: &str = ct_help_about!("arch.md");
-static CT_SUMMARY: &str = ct_help_section!("after help", "arch.md");
+use ctcore::ct_error::CTResult;
+use ctcore::ct_error::CtSimpleError;
+
+use ctcore::ct_format_usage;
+use ctcore::ct_help_about;
+use ctcore::ct_help_section;
+
+static ARCH_ABOUT: &str = ct_help_about!("arch.md");
+static ARCH_SUMMARY: &str = ct_help_section!("after help", "arch.md");
 #[ctcore::main]
 pub fn ctmain(args: impl ctcore::Args) -> CTResult<()> {
-    ct_main(args).map(|_| ())
+    arch_main(args).map(|_| ())
 }
 
-pub fn ct_main(args: impl ctcore::Args) -> CTResult<String> {
+pub fn arch_main(args: impl ctcore::Args) -> CTResult<String> {
     ct_app().try_get_matches_from(args)?;
 
-    let uts = PlatformInfo::new().map_err(|_e| CtSimpleError::new(1, "cannot get system name"))?;
+    let platform_info =
+        PlatformInfo::new().map_err(|_e| CtSimpleError::new(1, "cannot get system name"))?;
 
-    let binding = uts.machine().to_string_lossy();
+    let binding = platform_info.machine().to_string_lossy();
     let s = binding.trim();
-    println!("{}", uts.machine().to_string_lossy().trim());
+    println!("{}", platform_info.machine().to_string_lossy().trim());
     Ok(s.to_string())
 }
 
 pub fn ct_app() -> Command {
-    let utility_name = ctcore::ct_util_name();
+    let util_name = ctcore::ct_util_name();
     let command_version = crate_version!();
-    let application_info = CT_ABOUT;
-    let usage_description = ct_format_usage(CT_SUMMARY);
+    let application_info = ARCH_ABOUT;
+    let usage_description = ct_format_usage(ARCH_SUMMARY);
 
-    Command::new(utility_name)
+    Command::new(util_name)
         .version(command_version)
         .about(application_info)
         .after_help(usage_description)
@@ -114,7 +121,7 @@ mod tests {
         // println!("当前操作系统架构：{}", expected_arch);
 
         let args = vec![ctcore::ct_util_name()];
-        let result = ct_main(args.iter().map(|s| OsString::from(s)));
+        let result = arch_main(args.iter().map(|s| OsString::from(s)));
         let mut s = String::new();
         // 使用模式匹配提取字段值
         match result {

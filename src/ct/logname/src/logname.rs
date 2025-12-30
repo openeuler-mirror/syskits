84d6c7a541d3753887435caa1dd23f10d93aa73b
chenhr13 <chenhr13@chinatelecom.cn>
2025-12-30 10:05:48 +0800

    feat(logname): 添加显示当前用户登录名的工具

---
+++ src/ct/logname/src/logname.rs
@@ -0,0 +1,65 @@
+/*
+ * Copyright(c) 2022-2024 China Telecom Cloud Technologies Co., Ltd. All rights reserved.
+ *  syskits is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL V2
+ * You may obtain a copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ * KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+//! logname指令，它会显示目前用户的名称。
+
+use clap::{crate_version, Command};
+use ctcore::{ct_error::CTResult, ct_format_usage, ct_help_about, ct_help_usage, ct_show_error};
+use std::ffi::CStr;
+
+extern "C" {
+    // POSIX 要求使用 getlogin（或同等代码）
+    pub fn getlogin() -> *const libc::c_char;
+}
+
+fn get_user_login() -> Option<String> {
+    unsafe {
+        let login_name: *const libc::c_char = getlogin();
+        match login_name.is_null() {
+            true => None,
+            false => {
+                Some(String::from_utf8_lossy(CStr::from_ptr(login_name).to_bytes()).to_string())
+            }
+        }
+    }
+}
+
+const LOGNAME_ABOUT: &str = ct_help_about!("logname.md");
+const LOGNAME_USAGE: &str = ct_help_usage!("logname.md");
+
+#[ctcore::main]
+pub fn ctmain(args: impl ctcore::Args) -> CTResult<()> {
+    logname_main(args)
+}
+
+pub fn logname_main(args: impl ctcore::Args) -> CTResult<()> {
+    let _ = ct_app().try_get_matches_from(args)?;
+
+    match get_user_login() {
+        Some(userlogin) => println!("{userlogin}"),
+        None => ct_show_error!("no login name"),
+    }
+
+    Ok(())
+}
+
+pub fn ct_app() -> Command {
+    let utility_name = ctcore::ct_util_name();
+    let command_version = crate_version!();
+    let application_info = LOGNAME_ABOUT;
+    let usage_description = ct_format_usage(LOGNAME_USAGE);
+    Command::new(utility_name)
+        .version(command_version)
+        .about(application_info)
+        .override_usage(usage_description)
+        .infer_long_args(true)
+}
+
